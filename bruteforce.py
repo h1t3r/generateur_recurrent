b122726e95376abf60e05dd8e99662d4f62061c9
Chems <lahbabic@gmail.com>
2021-03-19 19:22:42 +0100

    enhance code

--- bruteforce.py
+++ bruteforce.py
@@ -3,15 +3,19 @@
 import string
 
 
-def setup_dict(length, tmp, tmp1, dict):
-    print length
-    length = length-1
-    if length == 1:
-        for x in tmp:
-            yield x
-    t = [x+y for x in tmp for y in tmp1]
-    for x in setup_dict(length, list(tmp), list(t), dict):
-        yield x
+def setup_dict(length, tmp):
+    tmp2 = []
+    tmp2 = list(tmp)
+    for i in range(length-1):
+        tmp1 = tmp2
+        for x in tmp1:
+            for y in tmp:
+                tmp2.append(x+y)
+                yield x+y
+
+
+
+
 
 def create_write_dict_to_file(length, dict_a, dict_n, dict_s):
     if dict_a and dict_n and dict_s:
@@ -22,24 +26,21 @@ def create_write_dict_to_file(length, dict_a, dict_n, dict_s):
         tmp = [ x for x in dict_a]
 
     file = open("dict.txt", "w+")
-    for x in setup_dict(length, list(tmp), list(tmp), list(tmp)):
-        print x+"\n"
+    for x in setup_dict(length, list(tmp)):
         file.write(x+"\n")
-"""
-    tmp = setup_dict(length, list(tmp), list(tmp), list(tmp))
-    for x in tmp:
         print x+"\n"
-        file.write(x+"\n")
-"""
+
+
+
 def dict_choice(choice):
     alph = "abcdefghijklmnopqrstuvwxyz"
     alph += string.upper(alph)
     num  = "0123456789"
     spec_chars = " &é\"'(§è!çà)-_^¨$*ù%`£+=/:.;?,"
     if choice == "alph_num":
-        create_write_dict_to_file(8, alph, num, "")
+        create_write_dict_to_file(6, alph, num, "")
     elif choice == "alph_num_spe-chars":
-        create_write_dict_to_file(8, alph, num, spec_chars)
+        create_write_dict_to_file(4, alph, num, spec_chars)
     elif choice == "alph":
         create_write_dict_to_file(14, alph, "", "")
 
