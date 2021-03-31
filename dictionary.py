cdbe93dcbb403dde1d6bc4684fad5bfa4c416ffd
Chems <lahbabic@gmail.com>
2021-03-31 18:50:16 +0200

    add a section: value length from x to x+length

--- dictionary.py
+++ dictionary.py
@@ -4,55 +4,63 @@ import string
 from optparse import OptionParser
 import sys
 
-def setup_dict(size, length, tmp):
+def setup_dict(size, startlength, length, tmp):
     tmp2 = list(tmp)
     length = int(length)
-    for i in range(length):
+    startlength = int(startlength)
+    for i in range(1, length+1):
         tmp1 = list(tmp2)
         tmp2 = []
-        for x in tmp:
+        for x in tmp1:
             for y in tmp:
                 tmp2.append(x+y)
                 if size == "full":
                     yield x+y
                 elif size == "length":
-                    if i == length-1:
+                    if i == length:
                         yield x+y
-                    else:
-                        yield ""
+                elif size == "section":
+                    if i >= startlength-1:
+                        yield x+y
 
 
-def create_write_dict_to_file(size, length, dict):
+
+def create_write_dict_to_file(size, startlength, length, dict):
     file = open("dict.txt", "w+")
-    for x in setup_dict(size, length, list(dict)):
-        if x != "":
-            file.write(x+"\n")
-            print x+"\n"
+    for x in setup_dict(size, startlength, length, list(dict)):
+        file.write(x+"\n")
+        print x+"\n"
 
 
 
-def dict_choice(size, length, type):
+def dict_choice(size, startlength, length, type):
     alph = "abcdefghijklmnopqrstuvwxyz"
     alph += string.upper(alph)
     num  = "0123456789"
     spec_chars = " &é\"'(§è!çà)-_^¨$*ù%`£+=/:.;?,|#@\\€"
     if type == "alph_num":
-        create_write_dict_to_file(size, length, alph+num)
+        create_write_dict_to_file(size, startlength, length, alph+num)
     elif type == "alph_num_spe-chars":
-        create_write_dict_to_file(size, length, alph+num+spec_chars)
+        create_write_dict_to_file(size, startlength, length, alph+num+spec_chars)
     elif type == "alph":
-        create_write_dict_to_file(size, length, alph)
+        create_write_dict_to_file(size, startlength, length, alph)
 
-def main(size, length, type):
-    dict_choice(size, length, type)
+def main(size, length, type, startlength):
+    dict_choice(size, startlength, length, type)
 
 if __name__ == '__main__':
     option_parser = OptionParser()
     option_parser.add_option("-s", "--size", dest="size",
-                      help="The size of the values of the dictionary.\n Either full dictionary it means the size is from 2 to length or just the length size. \n values: full | length")
+                      help="The size of the values of the dictionary.\n Full \
+                      dictionary it means the size is from 2 to length or just \
+                      the \"length\" size or a section from x to x+length. \n \
+                      values: full | section | length")
+
+    option_parser.add_option("-a", "--startlength", dest="startlength",
+                      help="From wich start the length of the dictionary.\n")
 
     option_parser.add_option("-l", "--length", dest="length",
-                      help="The length of the largest value.\n")
+                  help="The length of the largest value.\n")
 
     option_parser.add_option("-t", "--type", dest="type",
                       help="The type of the dictionary.\n Values: alph | alph_num | alph_num_spe-chars\n")
@@ -64,4 +72,5 @@ if __name__ == '__main__':
         print("\t"+sys.argv[0]+ " -h")
         exit("\n")
 
-    main(options.size, options.length, options.type)
+
+    main(options.size, options.length, options.type, options.startlength)
