ef4a0075d033717aa70d6adce5e0f57583de02fa
lahbabic <lahbabic@gmail.com>
2021-11-04 08:42:13 +0100

    v2

--- dictionary.py
+++ dictionary.py
@@ -26,27 +26,31 @@ def setup_dict(size, startlength, length, tmp):
 
 
 def create_write_dict_to_file(size, startlength, length, dict):
+    print("ok")
     file = open("dict.txt", "w+")
     for x in setup_dict(size, startlength, length, list(dict)):
         file.write(x+"\n")
-        print x+"\n"
+        print(x+"\n")
 
 
 
-def dict_choice(size, startlength, length, type):
+def dict_choice(size, startlength, length, dtype):
     alph = "abcdefghijklmnopqrstuvwxyz"
-    alph += string.upper(alph)
+    alph += alph.upper()
     num  = "0123456789"
     spec_chars = " &é\"'(§è!çà)-_^¨$*ù%`£+=/:.;?,|#@\\€"
-    if type == "alph_num":
+    print(dtype)
+    if dtype == "alph_num":
+        print("in")
         create_write_dict_to_file(size, startlength, length, alph+num)
-    elif type == "alph_num_spe-chars":
+    elif dtype == "alph_num_spe-chars":
         create_write_dict_to_file(size, startlength, length, alph+num+spec_chars)
-    elif type == "alph":
+    elif dtype == "alph":
         create_write_dict_to_file(size, startlength, length, alph)
 
-def main(size, length, type, startlength):
-    dict_choice(size, startlength, length, type)
+def main(size, length, dtype, startlength):
+    print("ok")
+    dict_choice(size, startlength, length, dtype)
 
 if __name__ == '__main__':
     option_parser = OptionParser()
@@ -62,15 +66,15 @@ if __name__ == '__main__':
     option_parser.add_option("-l", "--length", dest="length",
                   help="The length of the largest value.\n")
 
-    option_parser.add_option("-t", "--type", dest="type",
+    option_parser.add_option("-t", "--type", dest="dtype",
                       help="The type of the dictionary.\n Values: alph | alph_num | alph_num_spe-chars\n")
 
     (options, args) = option_parser.parse_args()
-    if options.size is None or options.length is None or options.type is None:
+    if options.size is None or options.length is None or options.dtype is None:
         print("Missing argument.")
         print("Please use -h for more information.")
         print("\t"+sys.argv[0]+ " -h")
         exit("\n")
 
 
-    main(options.size, options.length, options.type, options.startlength)
+    main(options.size, options.length, options.dtype, options.startlength)
