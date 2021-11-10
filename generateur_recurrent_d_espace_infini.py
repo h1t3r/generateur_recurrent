919dc75f7a1ce0a6161364e3bbf9f488dfed0d49
lahbabic <lahbabic@gmail.com>
2021-11-10 17:44:48 +0100

    little modifications

--- generateur_recurrent_d_espace_infini.py
+++ generateur_recurrent_d_espace_infini.py
@@ -1,16 +1,17 @@
 def generateur_recurrent_d_espace(alphabet_de_dimension,  coordonné, dimension):
 	for x in alphabet_de_dimension:
 		if len(coordonné)+1 < dimension:
-			for y in generateur_recurrent_d_espace(alphabet_de_dimension, coordonné + x, dimension):
+			for y in generateur_recurrent_d_espace(alphabet_de_dimension, str(coordonné) + str(x), dimension):
 				yield y
 		elif len(coordonné)+1 == dimension:
-			yield coordonné + x
+			yield coordonné + str(x)
 
 def generateur_recurrent_d_espace_infini():
 	i = 0
 	while True:
 		i = i + 1
-		for x in generateur_recurrent_d_espace([x for x in range(1, i)],  "", i):
+		for x in generateur_recurrent_d_espace([x for x in range(1, i+1)],  "", i):
 			yield x
 
-generateur_recurrent_d_espace_infini()
+for x in generateur_recurrent_d_espace_infini():
+	print(x)
