f928533b33d8c7135f0a64788edbfe9d9ecfdfeb
lahbabic <lahbabic@gmail.com>
2021-11-16 16:20:20 +0100

    ca marche

--- generateur_recurrent_d_espace_infini.py
+++ generateur_recurrent_d_espace_infini.py
@@ -6,12 +6,30 @@ def generateur_recurrent_d_espace(alphabet_de_dimension,  coordonné, dimension)
 		elif len(coordonné)+1 == dimension:
 			yield coordonné + str(x)
 
-def generateur_recurrent_d_espace_infini():
-	i = 0
-	while True:
-		i = i + 1
-		for x in generateur_recurrent_d_espace([x for x in range(1, i+1)],  "", i):
-			yield x
+def rentrer_dans_dimension(dim, a, l, c,alphabet):
+	if a < l:
+		print(a)
+		if isinstance(dim, list):
+			dim.append([[c+str(i)] for i in range(0, l+1)])
+			print(dim)
+			for x in dim:
+				for i in range(0, l+1):
+					print("i="+str(i))
+					print("a="+str(a))
+					for y in rentrer_dans_dimension(x, a+1, l ,c+str(i), alphabet):
+						yield y
+	elif a == l:
+		if isinstance(dim, list):
+			for i in range(0, l+1):	
+				dim.append(c+str(i))
+		print(dim)
+		yield dim
 
-for x in generateur_recurrent_d_espace_infini():
+
+
+
+for x in rentrer_dans_dimension([], 1, 3, "", [x for x in range(1, 4)]):
 	print(x)
+	print("\n")
+
+#generateur_recurrent_d_espace_2d()
