e3c2f6a4611640f8057766839d060bb72786d8ee
lahbabic <lahbabic@gmail.com>
2021-11-17 11:31:16 +0100

    update

--- generateur_recurrent_d_espace_infini.py
+++ generateur_recurrent_d_espace_infini.py
@@ -1,26 +1,22 @@
-def generateur_d_espace(dimension, a, coordonné, tmp, c, length_dimension):
+def generateur_d_espace(dimension, a, unité_de_dimension, coordonné,length_dimension):
 	if isinstance(dimension, list):
 		if a < length_dimension:
-			for i in coordonné:
-				dimension.append([tmp+str(i)])
+			for i in unité_de_dimension:
+				dimension.append([coordonné+str(i)])
 			for x in dimension:
-				if a == 1:
-					for y in generateur_d_espace(x, a+1, coordonné, tmp+x[c], c+1,length_dimension):
-						dimension[dimension.index(x)].append(y)
-				elif a > 1:
-					for y in generateur_d_espace(x, a+1, coordonné, x[0], c+1, length_dimension):
-						dimension[dimension.index(x)].append(y)
+				for y in generateur_d_espace(x, a+1, unité_de_dimension, x[0], length_dimension):
+					dimension[dimension.index(x)].append(y)
 			yield dimension
 		elif a == length_dimension:
-			for i in range(0, length_dimension):
-				dimension.append([tmp+str(i)])
+			for i in unité_de_dimension:
+				dimension.append([coordonné+str(i)])
 			yield dimension
 
 def generateur_d_espace_infini():
 	i = 0
 	while True:
 		i = i +1
-		for y in generateur_d_espace([], 1, [x for x in range(0, i)], "",0, i):
+		for y in generateur_d_espace([], 1, [x for x in range(0, i)], "", i):
 			yield y
 		if i == 3:
 			exit()
