5488568782e36d3f6f18b65b70009159625883c5
lahbabic <lahbabic@gmail.com>
2021-11-24 17:50:50 +0100

    generateur de dictionaire

--- generateur_recurrent_d_espace_infini.py
+++ generateur_recurrent_d_espace_infini.py
@@ -1,17 +1,14 @@
-def generateur_recurrent_d_espace(dimension, dimension_units, coordonnee, dimension_length):
-	if isinstance(dimension, list):
-		for i in dimension_units:
-			dimension.append([coordonnee+str(i)])
-		if len(coordonnee)+1 == dimension_length:
-			return dimension
-		for x in dimension:
-			for y in generateur_recurrent_d_espace(x, dimension_units, x[0], dimension_length):
-				pass
-		yield dimension
+def generateur_recurrent_d_espace(alphabet_de_dimension,  coordonné, dimension):
+	for i in alphabet_de_dimension:
+		if len(coordonné) < dimension-1:
+			for y in generateur_recurrent_d_espace(alphabet_de_dimension, coordonné + str(i), dimension):
+				yield y
+		elif len(coordonné) == dimension-1 :
+			yield coordonné + str(i)
 
 def generateur_recurrent_d_espace_infini():
 	i = 0
 	while True:
-		i = i +1
-		for y in generateur_recurrent_d_espace([], [x for x in range(0, i)], "", i):
-			yield y
+		i = i + 1
+		for x in generateur_recurrent_d_espace([x for x in range(1, i)],  "", i+1):
+			yield x
