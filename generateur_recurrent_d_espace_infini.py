2aa03f98f88d87b66df69f41434acef8ef70f7d8
lahbabic <lahbabic@gmail.com>
2021-12-11 15:27:04 +0100

    last version

--- generateur_recurrent_d_espace_infini.py
+++ generateur_recurrent_d_espace_infini.py
@@ -10,5 +10,5 @@ def generateur_recurrent_d_espace_infini():
 	i = 0
 	while True:
 		i = i + 1
-		for x in generateur_recurrent_d_espace([x for x in range(1, i)],  "", i+1):
+		for x in generateur_recurrent_d_espace([x for x in range(0, i)],  "", i):
 			yield x
