0ad3b9a1669a5e195a9dae36c7828aa5a63a98f0
lahbabic <lahbabic@gmail.com>
2021-12-19 16:52:48 +0100

    Without mutex.

--- generateur_recurrent_d_espace_infini.py
+++ generateur_recurrent_d_espace_infini.py
@@ -2,24 +2,19 @@ from find import find
 import threading
 
 dimension = []
-mutex = threading.Lock()
 
 def generateur_recurrent_d_espace(dimension0=[], dimension_units=[], coordonnee = "", dimension_length = 0):
     global dimension
     if isinstance(dimension0, list):
         if len(coordonnee)+1 < dimension_length:
             for i in dimension_units:
-                mutex.acquire()
                 dimension0.append([coordonnee+str(i)])
-                mutex.release()
             for x in dimension0:
                 y = threading.Thread(target=generateur_recurrent_d_espace, args=(x, dimension_units, x[0], dimension_length,))
                 y.run()
         elif len(coordonnee)+1 == dimension_length:
             for i in dimension_units:
-                mutex.acquire()
                 dimension0.append([coordonnee+str(i)])
-                mutex.release()
 
 def generateur_recurrent_d_espace_infini():
     global dimension
@@ -31,4 +26,5 @@ def generateur_recurrent_d_espace_infini():
         x.start()
         x.join()
         print(dimension)
+
 generateur_recurrent_d_espace_infini()
