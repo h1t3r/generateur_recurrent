5aacb8ecb3f4d530ec38071a75f6e11e683c6666
lahbabic <lahbabic@gmail.com>
2022-01-01 18:47:27 +0100

    Using memory

--- generateur_recurrent_d_espace_infini.py
+++ generateur_recurrent_d_espace_infini.py
@@ -1,16 +1,24 @@
-def generateur_recurrent_d_espace(alphabet_de_dimension,  coordonnee, dimension):
+espace = []
+
+def generateur_recurrent_d_espace(alphabet_de_dimension, dimension):
+    global espace
+    memory = list(espace)
+    espace = []
     for i in alphabet_de_dimension:
-        if len(coordonnee)+1 < dimension:
-            for y in generateur_recurrent_d_espace(alphabet_de_dimension, coordonnee + str(i), dimension):
-                yield y
-        elif len(coordonnee)+1 == dimension:
-            yield coordonnee + str(i)
+        if len(alphabet_de_dimension) > 2:
+            for x in memory:
+                espace.append((x[::-1]+str(i))[::-1])
+                yield  (x[::-1]+str(i))[::-1]
+        elif len(alphabet_de_dimension) == 2:
+            espace.append(str(i))
+            yield str(i)
 
 def generateur_recurrent_d_espace_infini():
     i = 0
+    global espace 
     while True:
         i = i + 1
-        for x in generateur_recurrent_d_espace([x for x in range(0, i)],  "", i):
+        for x in generateur_recurrent_d_espace([x for x in range(0, i)], i-1):
             yield x
 
 for x in generateur_recurrent_d_espace_infini():
