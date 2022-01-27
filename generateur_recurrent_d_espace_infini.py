791d02c674e1c6091bd05a4147551002e3e153f3
lahbabic <lahbabic@gmail.com>
2022-01-27 21:16:58 +0100

    Parallel.

--- generateur_recurrent_d_espace_infini.py
+++ generateur_recurrent_d_espace_infini.py
@@ -1,17 +1,40 @@
-def generateur_recurrent_d_espace(alphabet_de_dimension,  coordonnee, dimension):
-    for i in alphabet_de_dimension:
-        if len(coordonnee)+1 < dimension:
-            for y in generateur_recurrent_d_espace(alphabet_de_dimension, coordonnee + str(i), dimension):
-                yield y
-        elif len(coordonnee)+1 == dimension:
-            yield coordonnee + str(i)
+import threading
+import multiprocessing
+from time import sleep
+
+dimension = []
+
+def generateur_recurrent_d_espace(z, dimension_units=[], coordonnee = "", dimension_length = 0):
+    global dimension
+    if isinstance(dimension, list):
+        if len(coordonnee)+1 < dimension_length:
+            for i in dimension_units:
+                if z == []:
+                    dimension.append([coordonnee+str(i)])
+                else:
+                    z.append([coordonnee+str(i)])
+            if z == []:
+                for x in dimension:
+                    y = threading.Thread(target=generateur_recurrent_d_espace, args=(x, dimension_units, x[0], dimension_length,))
+                    y.start()
+                    print(dimension)
+            else:
+                for x in z:
+                    y = threading.Thread(target=generateur_recurrent_d_espace, args=(x, dimension_units, x[0], dimension_length,))
+                    y.start()
+        elif len(coordonnee)+1 == dimension_length:
+            for i in dimension_units:
+                z.append([coordonnee+str(i)])
 
 def generateur_recurrent_d_espace_infini():
+    global dimension
     i = 0
     while True:
-        i = i + 1
-        for x in generateur_recurrent_d_espace([x for x in range(0, i)],  "", i):
-            yield x
-
-for x in generateur_recurrent_d_espace_infini():
-    print(x)
+        dimension = []
+        i = i +1
+        x = threading.Thread(target=generateur_recurrent_d_espace, args=([], range(0, i), "", i,))
+        x.run()
+        print(dimension)
+        if i == 4:
+            exit()
+generateur_recurrent_d_espace_infini()
