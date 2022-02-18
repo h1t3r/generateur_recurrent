9feffc798b4b172183116bd124b59018cc202347
lahbabic <lahbabic@gmail.com>
2022-02-18 16:49:47 +0100

    last versio

--- generateur_recurrent_d_espace_infini.py
+++ generateur_recurrent_d_espace_infini.py
@@ -13,14 +13,13 @@ def generateur_recurrent_d_espace(dimension0=[], dimension_units=[], coordonnee 
                     dimension0.append([coordonnee+str(i)])
             if dimension0 == []:
                 for x in dimension:
-                    threads.append(threading.Thread(target=generateur_recurrent_d_espace, args=(x, dimension_units, x[0], dimension_length,)))
-                    threads[len(threads)-1].start()
-                for t in threads:
-                    t.join()
+                    y = threading.Thread(target=generateur_recurrent_d_espace, args=(x, dimension_units, x[0], dimension_length,))
+                    y.start()
+                    y.join()
             else:
                 for x in dimension0:
                     threads.append(threading.Thread(target=generateur_recurrent_d_espace, args=(x, dimension_units, x[0], dimension_length,)))
-                    threads[len(threads)-1].start()
+                    threads[len(threads)-1].run()
         elif len(coordonnee)+1 == dimension_length:
             for i in dimension_units:
                 dimension0.append([coordonnee+str(i)])
@@ -33,6 +32,4 @@ def generateur_recurrent_d_espace_infini():
         x = threading.Thread(target=generateur_recurrent_d_espace, args=([], range(0, i), "", i,))
         x.run()
         print(dimension)
-        if i == 4:
-            exit()
 generateur_recurrent_d_espace_infini()
